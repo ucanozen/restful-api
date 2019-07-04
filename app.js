175f29cc14309d12733e58f9c4d694582ec7bb42
Umut Ozen <ucanozen@gmail.com>
2019-07-04 13:18:14 -0700

    add morgan as logger and setup error handling route

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works!'
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
   });
 });
 
